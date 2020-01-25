313c53dc7a1a28f864a1d97a466801980aa8de8f
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-01-26 01:28:10 +0900

    Add new example: netuse_run2.go

---
+++ netuse_run2.go
@@ -0,0 +1,25 @@
+// +build run
+
+package main
+
+import (
+	"os"
+	"os/exec"
+
+	"github.com/zetamatta/go-windows-netresource"
+)
+
+func main() {
+	cancel, err := netresource.NetUse(`X:`, `\\localhost\C$`)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	defer cancel(true, false)
+
+	cmd := exec.Command("cmd.exe", "/c", "dir", `X:\`)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Run()
+}
