43063a7e4cd23a166b592fd07df3b457a08bb398
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-03-29 17:40:55 +0900

    make testcode for NewFileServer().Enum()

--- netresource_test.go
+++ netresource_test.go
@@ -1,7 +1,11 @@
 package netresource
 
 import (
+	"bufio"
+	"errors"
 	"os"
+	"os/exec"
+	"strings"
 	"testing"
 )
 
@@ -23,5 +27,76 @@ func TestNetresourceEnumFileServer(t *testing.T) {
 	}
 	if !found {
 		t.Fatalf("self computer name '%s' not found.", computerName)
+	}
+}
+
+func getOutputOfNetExeShare() ([]string, error) {
+	cmd := exec.Command("net", "share")
+	in, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	defer in.Close()
+	cmd.Start()
+
+	sc := bufio.NewScanner(in)
+	for sc.Scan() {
+		line := sc.Text()
+		if len(line) > 0 && line[0] == '-' {
+			nodes := []string{}
+			for sc.Scan() {
+				line = sc.Text()
+				if len(line) > 0 && line[0] == ' ' {
+					// skip continued line.
+					continue
+				}
+				field := strings.Fields(line)
+				if len(field) > 0 && !strings.ContainsRune(field[0], '$') {
+					nodes = append(nodes, field[0])
+				}
+			}
+			if len(nodes) >= 1 {
+				nodes = nodes[:len(nodes)-1]
+			}
+			return nodes, nil
+		}
+	}
+	return nil, errors.New("net share does not output as expected")
+}
+
+func TestNewFileServerEnum(t *testing.T) {
+	share, err := getOutputOfNetExeShare()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	computerName := `\\` + os.Getenv("COMPUTERNAME")
+	expected := make(map[string]struct{})
+	for _, s := range share {
+		expected[computerName+`\`+s] = struct{}{}
+	}
+	fs, err := NewFileServer(os.Getenv("COMPUTERNAME"))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	err = fs.Enum(func(nr *NetResource) bool {
+		rn := nr.RemoteName()
+		if _, ok := expected[rn]; ok {
+			delete(expected, rn)
+			// println(rn,": found")
+		} else {
+			t.Logf("%s is not found in expected list", rn)
+			t.Fail()
+		}
+		return true
+	})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if len(expected) > 0 {
+		t.Log("not all expected nodes are listuped")
+		for s, _ := range expected {
+			t.Log(s)
+		}
+		t.FailNow()
 	}
 }
